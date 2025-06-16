1c0915235a724312796d3a29d06055ccc9b2c7d3
SewerynMajewski <seweryn.majewski@gmail.com>
2025-06-16 08:35:42 +0200

    Cleanup config

--- config/config.js
+++ config/config.js
@@ -3,6 +3,5 @@ require('dotenv').config();
 module.exports = {
   mongoURI: process.env.MONGODB_URI || 'mongodb://localhost:27017/portfolio-manager',
   nbpApiUrl: 'http://api.nbp.pl/api/exchangerates/rates',
-  alphaVantageApiKey: process.env.ALPHA_VANTAGE_API_KEY,
   port: 5000
 };
